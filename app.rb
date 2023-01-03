2e88b059723dbee6dd7372698589080718013b5f
ahmednazirmusah <ahmed999nazir@gmail.com>
2023-01-03 17:26:36 +0000

    Add Load_data module to io_operations.rb

--- app.rb
+++ app.rb
@@ -1,12 +1,12 @@
 require_relative 'create_objects'
-
+require_relative 'io_operations'
 class App
   attr_reader :people, :books, :rentals
-
+  include Load_data
   def initialize
-    @people = []
-    @books = []
-    @rentals = []
+    @books = load_books
+    @people = load_people
+    @rentals = load_rentals
     @welcome_text = "\nPlease choose an option by entering a number:
     1- List all books
     2- List all people
@@ -16,19 +16,15 @@ class App
     6- List all rentals for a given person id
     7- Exit"
   end
-
   def add_person(person)
     @people.push(person)
   end
-
   def add_book(book)
     @books.push(book)
   end
-
   def add_rental(rental)
     @rentals.push(rental)
   end
-
   def list_books
     if @books.empty?
       puts 'Books list is empty! '
@@ -38,7 +34,6 @@ class App
       end
     end
   end
-
   def list_people
     if @people.empty?
       puts 'People list is empty! '
@@ -48,20 +43,17 @@ class App
       end
     end
   end
-
   def list_rentals
     print 'ID of person: '
     id = gets.chomp.to_i
     puts 'Rentals: '
     @people.each do |person|
       next unless person.id == id
-
       person.rentals.each do |rental|
         puts "Date: #{rental.date}, Book: #{rental.book.title} by #{rental.book.author}"
       end
     end
   end
-
   def pick_action(number)
     case number
     when 1
@@ -78,18 +70,17 @@ class App
       list_rentals
     end
   end
-
   def run
     puts 'Welcome to School Library App!'
     exit = false
     until exit
       puts @welcome_text
       number = gets.to_i
-
       if number < 7
         pick_action(number)
       elsif number == 7
         puts 'Thanks for using our library app, see you soon!'
+        save()
         exit = true
       else
         puts "Error friend, wrong input. Sorry I don't make the rules ¯\\(ツ)/¯"
--- io_operations.rb
+++ io_operations.rb
@@ -29,3 +29,70 @@ def save()
   end
 end
 
+module Load_data
+  def load_books
+    books = []
+    if File.exist?('./storage_files/books.json')
+      data = File.read("./storage_files/books.json")
+      if data != ""
+        JSON.parse(data).map do |book|
+          books.push(Book.new(book['title'], book['author']))
+        end
+      end
+    end
+  books
+end
+
+def load_people
+  people = []
+  if File.exist?('./storage_files/people.json')
+    data = File.read("./storage_files/people.json")
+      if data != ""
+        JSON.parse(data).map do |person|
+        if person['json_class'] == "Student"
+          people.push(Student.new(person['name'], person['age'], person['parent_permission']))
+        else
+          people.push(Teacher.new(person['name'], person['age'], person['specialization']))
+        end
+      end
+    end
+  end
+people
+end
+
+def load_rentals
+rentals = []
+if File.exist?('./storage_files/rentals.json')
+  data = File.read("./storage_files/rentals.json")
+      if data != ""
+        JSON.parse(data).map do |rental|
+          puts rental
+          person_id = rental['person']['id']
+          book_id = rental['book']['id']
+          puts rental['date']
+          puts get_book(book_id)
+          puts get_person(person_id)
+          rentals.push(Rental.new(get_person(person_id), get_book(book_id), rental['date']))
+        end
+      end
+end
+rentals
+end
+def get_person(id)
+  # @people.select {|person| person.id == id}[0]
+  @people.each do |person|
+    if person.id == id
+      puts person.id
+    end
+  end
+  person_needed
+  end
+def get_book(id)
+  # @books.select {|book| book.id == id}[0]
+  @books.each do |book|
+    if book.id == id
+      book
+    end
+  end
+end
+end
