f4c11e72b27cdb32e5dae6beb040da95d1e911b6
Hayet Fellah <hayet.fellah145@gmail.com>
2023-01-03 22:13:52 +0100

    Fix LoadData Module and create SaveData module

--- io_operations.rb
+++ io_operations.rb
@@ -4,95 +4,89 @@ require_relative 'rental'
 require_relative 'student'
 require_relative 'teacher'
 
-def save()
-  file_names = ['books.json', 'people.json', 'rentals.json']
-  file_names.each do |name| 
-    if File.exist?('./storage_files/'+ name)
-      case name
-      when 'books.json'
-        if @books.any?
-          books_data = JSON.generate(@books, {max_nesting: false})
-          File.write("./storage_files/books.json", books_data)
-        end
-      when 'people.json'
-        if @people.any? 
-          people_data = JSON.generate(@people, {max_nesting: false})
-          File.write("./storage_files/people.json", people_data)
-        end 
-      when 'rentals.json'
-        if @rentals.any?
-          rentals_data = JSON.generate(@rentals, {max_nesting: false})
-          File.write("./storage_files/rentals.json", rentals_data)
-        end
-      end
-    end
+module SaveData
+  def save_books
+    return unless File.exist?('./storage_files/books.json')
+    return unless @books.any?
+
+    books_data = JSON.generate(@books, { max_nesting: false })
+    File.write('./storage_files/books.json', books_data)
+  end
+
+  def save_people
+    return unless File.exist?('./storage_files/people.json')
+    return unless @people.any?
+
+    people_data = JSON.generate(@people, { max_nesting: false })
+    File.write('./storage_files/people.json', people_data)
+  end
+
+  def save_rentals
+    return unless File.exist?('./storage_files/rentals.json')
+    return unless @rentals.any?
+
+    rentals_data = JSON.generate(@rentals, { max_nesting: false })
+    File.write('./storage_files/rentals.json', rentals_data)
+  end
+
+  def save()
+    save_books
+    save_people
+    save_rentals
   end
 end
 
-module Load_data
+module LoadData
   def load_books
     books = []
     if File.exist?('./storage_files/books.json')
-      data = File.read("./storage_files/books.json")
-      if data != ""
+      data = File.read('./storage_files/books.json')
+      if data != ''
         JSON.parse(data).map do |book|
-          books.push(Book.new(book['title'], book['author']))
+          books.push(Book.new(book['title'], book['author'], book['id']))
         end
       end
     end
-  books
-end
-
-def load_people
-  people = []
-  if File.exist?('./storage_files/people.json')
-    data = File.read("./storage_files/people.json")
-      if data != ""
-        JSON.parse(data).map do |person|
-        if person['json_class'] == "Student"
-          people.push(Student.new(person['name'], person['age'], person['parent_permission']))
-        else
-          people.push(Teacher.new(person['name'], person['age'], person['specialization']))
-        end
-      end
-    end
+    books
   end
-people
-end
 
-def load_rentals
-rentals = []
-if File.exist?('./storage_files/rentals.json')
-  data = File.read("./storage_files/rentals.json")
-      if data != ""
+  def load_people
+    people = []
+    if File.exist?('./storage_files/people.json')
+      data = File.read('./storage_files/people.json')
+      if data != ''
+        JSON.parse(data).map do |person|
+          if person['json_class'] == 'Student'
+            people.push(Student.new(person['name'], person['age'], person['parent_permission'], person['id']))
+          else
+            people.push(Teacher.new(person['name'], person['age'], person['specialization'], person['id']))
+          end
+        end
+      end
+    end
+    people
+  end
+
+  def load_rentals
+    rentals = []
+    if File.exist?('./storage_files/rentals.json')
+      data = File.read('./storage_files/rentals.json')
+      if data != ''
         JSON.parse(data).map do |rental|
-          puts rental
           person_id = rental['person']['id']
           book_id = rental['book']['id']
-          puts rental['date']
-          puts get_book(book_id)
-          puts get_person(person_id)
           rentals.push(Rental.new(get_person(person_id), get_book(book_id), rental['date']))
         end
       end
-end
-rentals
-end
-def get_person(id)
-  # @people.select {|person| person.id == id}[0]
-  @people.each do |person|
-    if person.id == id
-      puts person.id
     end
+    rentals
   end
-  person_needed
+
+  def get_person(id)
+    @people.find { |person| person.id == id }
   end
-def get_book(id)
-  # @books.select {|book| book.id == id}[0]
-  @books.each do |book|
-    if book.id == id
-      book
-    end
+
+  def get_book(id)
+    @books.find { |book| book.id == id }
   end
-end
 end
